a2ff777889be4036b1182820d08e210675e226c5
0x42 <bpozdnyakov@gmail.com>
2014-06-25 18:11:24 +0400

    fin www.westrags.com.js and fix bug(read all url images in product ) in www.gucci.com.js  On branch origin/parseurl  Changes to be committed: 	modified:   gucci.js 	modified:   westrags.js 	modified:   www.gucci.com.js 	modified:   www.westrags.com.js

--- westrags.js
+++ westrags.js
@@ -16,20 +16,18 @@ var urlProduct2 = 'http://www.westrags.com/en/loja/homem/8-textil/1067-tee-slim/
 var urlProduct3 = "http://www.westrags.com/en/loja/acessorios/208177-seiko-velatura-spc075p1-detail.html";
 //sell price
 var urlProduct4 = 'http://www.westrags.com/en/loja/homem/36-calcado/62-botas-e-botins/65170-botin--eylets-pepe-jeans-london-detail.html';
-var urlProduct5 = 'http://www.westrags.com/en/loja/acessorios/208131-guess-fierce-gw0001l2-detail.html';
-var urlProduct6 = 'http://www.westrags.com/pt/loja/crianca/146-calcado/91435-havaianas-chinelo-kids-fun-detail.html';
+var urlProduct5 = 'http://www.westrags.com/pt/loja/mulher/10-textil/21-t-shirts/191665-boombap-lookback-tee-boat--women-detail.html';
+var urlProduct6 = 'http://www.westrags.com/en/loja/25178-camisola-selected-detail.html';
 // test
 var urlCat = "http://www.westrags.com/en/";
 var urlCat2 = "http://www.westrags.com/en/loja/mulher";
 var urlCat3 = 'http://www.westrags.com/pt/loja/acessorios/1105-relogios';
-var result = arachnid.inspectUrl(siteId, urlProduct6, parser);
-
+// var result = arachnid.inspectUrl(siteId, urlProduct6, parser);
 // var uuu =  arachnid.inspectEntranceUrls(siteId, parser);
-// var result = arachnid.crawl(siteId, parser);
-   p("urls:");
-   p(result[0]);
-   p(result[0].length);
-   p("product:");
-   p(result[1]); 
-   process.exit();
+var result = arachnid.crawl(siteId, parser);
+   // p("urls:");
+   // p(result[0]);
+   // p("product:");
+   // p(result[1]); 
+   // process.exit();
 });
--- www.westrags.com.js
+++ www.westrags.com.js
@@ -1,7 +1,7 @@
  /*jshint node: true, indent:2, loopfunc: true, asi: true, undef:true*/
 /*global app: false, jQuery: false, $: false, window: false, _: false, document: false*/
 "use strict";
-exports.siteId = 'http://www.westrags.com/pt/'
+exports.siteId = 'http://www.westrags.com/'
 
 
 exports.entranceUrls = function(page){
@@ -68,92 +68,95 @@ exports.parse = function(url, page, saveFile) {
     	}
     	return color;
     });
-	
 	product.variations = [];
-	product.images = [];    
-	for(var i = 0; i < v.length; i++) {
-		var imgSrc;
-		if(v.length == 1) {
-			imgSrc = page.evaluate(function(tag) {
-	    		var src = null;
-    	  		var n = jQuery('#product_colors>div');
-	    	    jQuery(n[tag]).click();
-    	  		var img = jQuery(".imagemesquerda>a");
-    	  		if(img.length > 0) {
-    	  			src = img[0].href;
-    	  		}
-	    	  return src;
-	    	}, i);
-		} else {
-			imgSrc = page.evaluate(function(tag) {
-	    	  var src = null;
-	    	  var n = jQuery('#product_colors>div');
-	    	  jQuery(n[tag]).click();
-	    	  if(jQuery(n[tag]).hasClass("with_image")) {
-	    	  	var img = jQuery(".imagemesquerda>a");
-	    	  	if(img.length > 0) {
-	    	  		src = img[0].href;
-	    	  	}
-	    	  }
-	    	  return src;
-	    	}, i);	
-		}
-
-    	if(imgSrc !== null) {
-    		var save = saveFile(product.id, imgSrc);
-        	product.images.push({id:save[0], url: save[1], originalUrl: product.url});
-    	}
-    	var items = page.evaluate(function(e) {
-    		var ans = {};
-    		// color
-    		var c = jQuery("#product_colors_name>span").text();
-    		// price
-    		var  p = jQuery("span.PricebasePriceWithTax");
-    		var pTxt = null;
-    		var psTxt = null;
-    		if( p.length > 0 ) {
-    			pTxt = jQuery("span.PricebasePriceWithTax").text();
-    			psTxt = jQuery("span.PricesalesPrice").text();
-    		} else {
-    			pTxt = jQuery("span.PricesalesPrice").text();
-    		}
-    		if(pTxt !== null) {
-    			var buf = pTxt.split(" ");
-    			var b = buf[1].split(",");
-    			ans.price = window.parseFloat(b[0]+"."+b[1]);
-    		}
-    		if(psTxt !== null) {
-    			var buf2 = psTxt.split(" ");
-    			var b2 = buf2[1].split(",");
-    			ans.sellPrice = window.parseFloat(b2[0]+"."+b2[1]);	
-    		}
-    		// size
-    		var sArr = jQuery("#product_size>div:visible");
-    		var s = [];
-    		for(var sI = 0; sI < sArr.length; sI++) {
-    			s.push(jQuery(sArr[sI]).text());
-    		}
-        if(c === "") c = "no color";
-    		ans.color = c;
-    		// ans.push(p);
-    		ans.size = s;
-    		return ans;
-    	});
-    	for(var it=0; it < items.size.length; it++) {
-    		var buf = {}; 
-    		buf.size = items.size[it];
-    		buf.color = items.color; 
-    		buf.price = items.price;
-    		buf.available = true;
-    		if(items.sellPrice !== undefined)
-    			buf.sellPrice = items.sellPrice;
-    		product.variations.push(buf);	
-    	} 
-    }
-    // description
-    product.description = page.evaluate(function(e){
-    	return jQuery("#descricao").text();
+	product.images = [];
+  var addImg = page.evaluate(function(){
+    var ans = null;
+    var allImg = jQuery(".additional-images>a");  
+    if(allImg !== undefined && allImg.length > 0 ) {
+      ans = [];
+      for(var i = 0; i < allImg.length; i++) {
+        ans.push(allImg[i].href);
+      }
+    }  
+    return ans;
+  });
+  if(addImg === null) {
+    addImg = page.evaluate(function(){
+      var ans = [];
+      var img = jQuery(".imagemesquerda>a");
+      if(img.length > 0) {
+        ans.push(img[0].href);
+      }
+      return ans;
     });
+  }
+  
+  if(addImg.length > 0) {
+    for(var i = 0; i < addImg.length; i++) {
+      var save = saveFile(product.id, addImg[i]);
+      product.images.push({id:save[0], url: save[1], originalUrl: product.url});  
+    }
+  }  
+
+	for(var i = 0; i < v.length; i++) {
+		page.evaluate(function(tag) {
+    	  var n = jQuery('#product_colors>div');
+    	  jQuery(n[tag]).click();
+  	}, i);	
+
+  	var items = page.evaluate(function(e) {
+  		var ans = {};
+  		// color
+  		var c = jQuery("#product_colors_name>span").text();
+  		// price
+  		var  p = jQuery("span.PricebasePriceWithTax");
+  		var pTxt = null;
+  		var psTxt = null;
+  		if( p.length > 0 ) {
+  			pTxt = jQuery("span.PricebasePriceWithTax").text();
+  			psTxt = jQuery("span.PricesalesPrice").text();
+  		} else {
+  			pTxt = jQuery("span.PricesalesPrice").text();
+  		}
+  		if(pTxt !== null) {
+  			var buf = pTxt.split(" ");
+  			var b = buf[1].split(",");
+  			ans.price = window.parseFloat(b[0]+"."+b[1]);
+  		}
+  		if(psTxt !== null) {
+  			var buf2 = psTxt.split(" ");
+  			var b2 = buf2[1].split(",");
+  			ans.sellPrice = window.parseFloat(b2[0]+"."+b2[1]);	
+  		}
+  		// size
+  		var sArr = jQuery("#product_size>div:visible");
+  		var s = [];
+  		for(var sI = 0; sI < sArr.length; sI++) {
+  			s.push(jQuery(sArr[sI]).text());
+  		}
+      if(c === "") c = "no color";
+  		ans.color = c;
+  		// ans.push(p);
+  		ans.size = s;
+  		return ans;
+  	});
+  	for(var it=0; it < items.size.length; it++) {
+  		var buf = {}; 
+  		buf.size = items.size[it];
+      if(buf.size === '') buf.size = '0';
+  		buf.color = items.color; 
+  		buf.price = items.price;
+  		buf.available = true;
+  		if(items.sellPrice !== undefined)
+  			buf.sellPrice = items.sellPrice;
+  		product.variations.push(buf);	
+  	} 
+  }
+    // description
+  product.description = page.evaluate(function(e){
+  	return jQuery("#descricao").text();
+  });
 
   } else {
     // check start page or product catalog
